b9ed3cb656b9269b795de7c432add76fc006cb50
Declan Whelan <dwhelan@dpwhelan.com>
2018-04-09 12:06:17 -0500

    Added Huex dependency & removed factorial code

--- mix.exs
+++ mix.exs
@@ -14,14 +14,16 @@ defmodule Huey.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :httpoison]
+
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mix_test_watch, "~> 0.5"}
+      {:mix_test_watch, "~> 0.5"},
+      {:huex, "~> 0.7"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
--- test/huey_test.exs
+++ test/huey_test.exs
@@ -2,23 +2,5 @@ defmodule HueyTest do
   use ExUnit.Case
   doctest Huey
 
-  test "greets the world" do
-    assert Huey.hello() == :world
-  end
-
-  test "fact(0) should be 1" do
-    assert Huey.fact(0) == 1
-  end
-
-  test "fact(1) should be 1" do
-    assert Huey.fact(1) == 1
-  end
-
-  test "fact(3) should be 6" do
-    assert Huey.fact(3) == 6
-  end
-
-  test "fact(4) should be 24" do
-    assert Huey.fact(4) == 24
-  end
+# xOYF2J7wjNk7SP2ddvqJLCwa10I8OTQO5R1hVofb
 end
