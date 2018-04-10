f91dcc6b0b6f028dd2d152fd14fbd64db5f23343
Declan Whelan <dwhelan@dpwhelan.com>
2018-04-10 13:16:56 -0500

    Design comments for http server

--- lib/huey/scene.ex
+++ lib/huey/scene.ex
@@ -6,5 +6,12 @@ defmodule Huey.Scene do
   defstruct [name: "", light_states: []]
 end
 
+# Create scene
 # POST http://host/scene/blue_jays - create a scene
+#   {} post contains json => Elixir data structure
+#   http server -
+#
+# Activate scene
+# POST http://host/scene/blue_jays/activate - activates a scene
+#
 # GET  http://host/scene/blue_jays - retrieve a scene
